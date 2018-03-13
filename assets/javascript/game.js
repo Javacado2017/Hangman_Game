2a33e63844691a78f71612ffa4489d71ce48623d
unknown <newtonsapple2017@gmail.com>
2018-03-13 16:02:39 -0400

    javascript homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,141 +1,137 @@
-<script>
+window.onload = function () {
 
-// Randomly select category and display category on screen
-var categoryOptions = ["Category1", "Category2", "Category3"];
-var selectCategory = categoryOptions[Math.floor(Math.random()*categoryOptions.length)];
+    // Randomly select category and display category on screen
+        var categoryOptions = ["CategoryOne", "CategoryTwo", "CategoryThree"];
+        var selectCategory = categoryOptions[Math.floor(Math.random()*categoryOptions.length)];
 
-document.getElementById("myCategory").textContent = selectCategory;
+        document.getElementById("myCategory").textContent = selectCategory;
 
-// Select set of words from category
-var Category1 = ["word1", "word2", "word3", "word4", "word5"];
-var Category2 = ["word6", "word7", "word8", "word9", "word10"];
-var Category3 = ["word11", "word12", "word13", "word14", "word15"];
+    // Randomly select word from category split word into array
+        var Category1 = ["wordone", "wordtwo", "wordthree", "wordfour", "wordfive"];
+        var Category2 = ["wordsix", "wordseven", "wordeight", "wordnine", "wordten"];
+        var Category3 = ["wordeleven", "wordtwelve", "wordthirteen", "wordfourteen", "wordfifhteen"];
 
-if (categoryOptions = "Category1") {
-    var wordOptions = Category1;
-};
+        if (categoryOptions = "CategoryOne") {
+            var wordOptions = Category1;
+        };
 
-if (categoryOptions = "Category2") {
-    var wordOptions = Category2;
-};
+        if (categoryOptions = "CategoryTwo") {
+            var wordOptions = Category2;
+        };
 
-   if (categoryOptions = "Category3") {
-    var wordOptions = Category3;
-};
+        if (categoryOptions = "CategoryThree") {
+            var wordOptions = Category3;
+        };
 
+        var selectWord = wordOptions[Math.floor(Math.random()*wordOptions.length)];
+        var splitWord = selectWord.split("");
 
+    // Set lives equal to 10 and display on screen
+        var lives = 10; 
+        var newDiv = document.createElement("div");
+        newDiv.textContent = lives;
+        document.getElementById("myLives").appendChild(newDiv);
 
+    // Set games won equal to 0 and display on screen
+        var gamesWon = 0; 
+        var newDiv = document.createElement("div");
+        newDiv.textContent = gamesWon;
+        document.getElementById("gamesWon").appendChild(newDiv);
 
-//Set lives equal to 10 and display in document
-var lives = 10; 
-var newDiv = document.createElement("div");
-newDiv.textContent = "Lives left: " + lives;
-document.getElementById("myLives").appendChild(newDiv);
+    // Set games lost equal to 0 and display on screen
+        var gamesLost = 0; 
+        var newDiv = document.createElement("div");
+        newDiv.textContent = gamesLost;
+        document.getElementById("gamesLost").appendChild(newDiv);
 
+    // Create alphabet array to compare guesses to and display on screen
+        var alphabetArray = [];
 
-
-
-
-
-//Create alphabet to compare to
-var alphabetArray = [];
-
-for (i = 0; i < 26; i++) {
-alphabetArray[i] = (i+10).toString(36);   
-}; 
-//console.log(alphabetArray);
-var newDiv = document.createElement("div");
-newDiv.textcontent = alphabetArray;
-document.getElementById("lettersRemaining").innerHTML = alphabetArray.toString();
-
-
-
-
-
-// Randomly select word, split word into array
-var selectWord = wordOptions[Math.floor(Math.random()*wordOptions.length)];
-var splitWord = selectWord.split("");
-
-
-
-
-// Create blanksSpaces array to compare SplitWord[i] and blankSpace[i] later on
-var blankSpaces = [];
-for (i = 0; i < splitWord.length; i++) {
-    blankSpaces[i] = ("_");
-}
-console.log(splitWord);
-console.log(blankSpaces);
-
-//Create "_" in HTML to be replaced later on
-blankSpaces.forEach(function(blankSpaces) {
-var newDiv = document.createElement("div");
-newDiv.textContent = blankSpaces;
-document.getElementById("blankSpaces").appendChild(newDiv);
-});
-
-// Function to run each time player enters a key
-var lettersGuessed = [];
-
-document.onkeyup = function(event) {
-    var userGuess = event.key.toLowerCase();
-    //console.log(userGuess);
-    
-    var alreadyGuessed = lettersGuessed.indexOf(userGuess);
-    
-    if (alreadyGuessed >= 0){
-        alert("You already typed that letter. Guess again...");
-        return;
-    }; 
-    
-    else (alphabetArray.indexOf(userGuess) < 0) {
-        alert("What alphabet system do you write with? That's not a letter. Guess again...");
-        return;
-    };
-
-
-    lettersGuessed.push(userGuess);
-    alphabetArray.splice(alphabetArray.indexOf(userGuess),1);
-
-    var newDiv1 = document.createElement("div");
-    var newDiv2 = document.createElement("div");
-    newDiv1.textContent = lettersGuessed;
-    newDiv2.textContent = alphabetArray;
-    document.getElementById("lettersGuessed").innerHTML = lettersGuessed.toString();
-    document.getElementById("lettersRemaining").innerHTML = alphabetArray.toString();
-
-    if (splitWord.indexOf(userGuess) >=0) {
-        playerProgress(userGuess);
-        return;
-    }
-
-    else (splitWor.indxOf(userGuess) < 0) {
-
-
-
-    };
+        for (i = 0; i < 26; i++) {
+        alphabetArray[i] = (i+10).toString(36);   
+        }; 
         
+        var newDiv = document.createElement("div");
+        newDiv.textcontent = alphabetArray;
+        document.getElementById("lettersRemaining").innerHTML = alphabetArray.toString();
 
-};
+    // Create number of blanks spaces for ever letter in word
+        var blankSpaces = [];
+        for (i = 0; i < splitWord.length; i++) {
+            blankSpaces[i] = ("_");
+        };
+        
+        var newDiv = document.createElement("div");
+        newDiv.textContent = blankSpaces;
+        document.getElementById("blankSpaces").innerHTML = blankSpaces.toString();
+        console.log(blankSpaces);
 
-function playerProgress (letter) {
-    for (i = 0; i < splitWord.length; i++) {
-        if (splitWord[i] === letter) {
-            blankSpaces[i] = letter;
-            document.getElementById("blankSpaces").children[i].innerHTML = letter;
-        }
-    };
+    // Function to run each time player enters a key  
+        var lettersGuessed = [];
+
+        document.onload = function (event) {
+            var userGuess = event.key.toLowerCase();
+
+        // Alert user if they entered same letter twice or if the guess is not a letter
+            var alreadyGuessed = lettersGuessed.indexOf(userGuess);
+            
+                if (alreadyGuessed >= 0){
+                    alert("You already typed that letter. Guess again.");
+                    return;
+                }
+                
+                else if (alphabetArray.indexOf(userGuess) < 0) {
+                    alert("That's not a letter. Guess again.");
+                    return;
+                };
+
+        // Push the guesses to an array and remove it from the remaining letters list
+            lettersGuessed.push(userGuess);
+            alphabetArray.splice(alphabetArray.indexOf(userGuess),1);
+
+            var newDiv1 = document.createElement("div");
+            var newDiv2 = document.createElement("div");
+            newDiv1.textContent = lettersGuessed;
+            newDiv2.textContent = alphabetArray;
+            document.getElementById("lettersGuessed").innerHTML = lettersGuessed.toString();
+            document.getElementById("lettersRemaining").innerHTML = alphabetArray.toString();
+        
+        // Run playerProgress function when player has guessed a correct letter otherwise deduct a life
+            if (splitWord.indexOf(userGuess) >=0) {
+                playerProgress(userGuess);
+            }
+
+            else {
+                lives--;
+                document.getElementById("myLives").innerHTML = lives.toString();
+            }
+        
+        // If player lives goes to zero, player loses game and game restarts
+            if (lives === 0) {
+                gamesLost++;
+                alert("No more guesses remaining. You lost this game. The word was " + selectWord + ". Click 'Okay' to play again.");
+                document.getElementById("gamesLost").innerHTML = gamesLost.toString();  
+            };
+        
+        // If player guessed the word, player wins game and game restarts
+            if (document.getElementById("blankSpaces").innerHTML.indexOf("_") === -1) { 
+                gamesWon++;
+                alert("You won. Click 'Okay' to play again.")
+                document.getElementById("gamesWon").innerHTML = gamesWon.toString();
+            };
+        };
+
+    // playerProgress function to search letter splitWord array and replace it with guessed letter or deduct a life if not fouind    
+
+        function playerProgress (letter) {
+            for (i = 0; i < splitWord.length; i++) {
+                if (splitWord[i] === letter) {
+                    blankSpaces[i] = letter;
+                    document.getElementById("blankSpaces").innerHTML = blankSpaces.toString();
+                }
+            };
+            
+        };
 }
 
-
-
-
-
-
-
-
-
-
-
-
-</script>
+// Missing ... how to restard game and keep track of losses and wins 
